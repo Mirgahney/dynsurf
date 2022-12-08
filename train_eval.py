da6eff07ba5722168dbabc21c482ef2cdd30d7e8
Mirgahney Mohamed <mmerghaney@aimsammi.org>
2022-12-08 12:06:46 +0000

    unify depth/intrinsics_paras from dataset, minor modifications

--- train_eval.py
+++ train_eval.py
@@ -23,7 +23,7 @@ os.system('python -u exp_runner.py --mode validate_mesh --conf ./confs/{} --is_c
 # os.system('python -u exp_runner.py --mode validate_mesh --conf ./confs/{} --is_continue --case {} --gpu {}'.format(confname,casename,gpuname))
 
 # KillingFusion Duck
-# confname='kfusion_snoopy.conf'
+# confname='snoopy_ndr.conf'
 # casename='kfusion_snoopy'
 # os.system('python -u exp_runner.py --mode train --conf ./confs/{} --case {} --gpu {}'.format(confname, casename, gpuname))
 # os.system('python -u exp_runner.py --mode validate_mesh --conf ./confs/{} --is_continue --case {} --gpu {}'.format(confname,casename,gpuname))
